c24e0636b3105b5f73441695294779c055304da9
Ruwani <ruwani054@gmail.com>
2024-06-30 14:48:45 +0530

    vpc created

--- root/backend.tf
+++ root/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-ru-101"
-    key    = "backend/terraform-pactice-project.tfstate"
-    region = "ap-southeast-1"
+    bucket         = "tfstate-ru-1012"
+    key            = "backend/terraform-pactice-project.tfstate"
+    region         = "ap-southeast-1"
     dynamodb_table = "remote-backend"
   }
 }
--- root/variables.tf
+++ root/variables.tf
@@ -1,5 +1,14 @@
-variable project_name {
+variable "project_name" {
   description = "custom modules in tf"
 }
 
-variable region {}
+variable "region" {}
+variable "vpc_cidr" {}
+variable "pub_sub_1a_cidr" {}
+variable "pub_sub_2b_cidr" {}
+variable "pri_sub_3a_cidr" {}
+variable "pri_sub_4b_cidr" {}
+variable "pri_sub_5a_cidr" {}
+variable "pri_sub_6b_cidr" {}
+variable "db_username" {}
+variable "db_password" {}
