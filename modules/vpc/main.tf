95fa0c9f36be63e97d1359583f51c025f6e6fac2
Ruwani <ruwani054@gmail.com>
2024-06-30 15:14:54 +0530

    public subnet and nat gateway added

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -18,3 +18,54 @@ resource "aws_internet_gateway" "internet_gateway" {
     Name = "${var.project_name}-igw"
   }
 }
+
+// use data source to get all availability zones in region
+
+data "aws_availability_zones" "availability_zones" {}
+
+# create public subnet pub_sub_1a
+resource "aws_subnet" "pub_sub_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.pub_sub_1a_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch = true
+
+  tags      = {
+    Name    = "pub_sub_1a"
+  }
+}
+
+# create public subnet pub_sub_2b
+resource "aws_subnet" "pub_sub_2b" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.pub_sub_2b_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch = true
+
+  tags      = {
+    Name    = "pub_sub_2b"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "pub_sub_1a_route_table_association" {
+  subnet_id      = aws_subnet.pub_sub_1a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "pub_sub_2b_route_table_association" {
+  subnet_id      = aws_subnet.pub_sub_2b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
